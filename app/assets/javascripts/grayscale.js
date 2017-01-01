3adb838e12f32b895ad156217eba842db9156a08
Petya Nikolova <petia_182@yahoo.com>
2017-01-01 19:28:57 +0200

    fixed scroll

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -6,7 +6,7 @@
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 1) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
