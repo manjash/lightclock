5519ede7aa35a2be67b92c201bb5a8ebdb7146a9
Petya Nikolova <petia_182@yahoo.com>
2017-01-26 12:56:36 +0000

    added 404 escape page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :show ]
 
   def show
-    render template: "pages/#{params[:page]}"
+    if valid_page?
+      render template: "pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
   end
 
 end
