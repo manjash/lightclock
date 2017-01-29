551ce7e9e03b57874d1f46aa36876dba0b5a4a28
Petya Nikolova <petia_182@yahoo.com>
2017-01-29 21:11:38 +0000

    removed validation for website

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < MailForm::Base
 
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :website   #:validate => /https?:\/\/[\S]+/
+  attribute :website
   attribute :field,     :validate => true
   attribute :degree,    :validate => true
   attribute :file,      :attachment => true
