46dcc4d4236857ccedf9dc2ea55fa9394cde3717
Petya Nikolova <petia_182@yahoo.com>
2017-01-29 14:04:58 +0000

    trying to make attachment work

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,12 +2,12 @@ class Contact < MailForm::Base
 
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message   # :validate => true
-  attribute :something   #   :validate => true
-  # attribute :website
+  attribute :website   #:validate => /https?:\/\/[\S]+/
+  attribute :field,     :validate => true
+  attribute :degree,    :validate => true
+  attribute :file,      :attachment => true
   attribute :nickname,  :captcha => true
   attribute :file,      :attachment => true
-  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
