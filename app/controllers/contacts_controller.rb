8f306772c9e566b2093acb81aa9c191930ce5643
Petya Nikolova <petia_182@yahoo.com>
2017-02-01 23:20:46 +0000

    kinda fixed navbar links on hover

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,6 +11,12 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
+      open(URI::encode("http://light-clock-platform.herokuapp.com/thinker_profiles/add?name=
+        #{@contact.name}&email=#{@contact.email}
+        &expertise=#{@contact.expertise}
+        &degree=#{@contact.degree}
+        &website=#{@contact.website}
+        &cv_url=#{@contact.cv_url}"))
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
       redirect_to root_path, notice: "Your form was succesfully submitted!"
     else
