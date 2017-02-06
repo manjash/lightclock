762f68f2a238d522ac178605692a21fe42680234
Petya Nikolova <petia_182@yahoo.com>
2017-02-06 20:39:42 +0000

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   ActiveAdmin.routes(self)
-  get 'contacts/new'
-
-  get 'contacts/create'
-
-  get 'messages/new'
-  get 'messages/create'
-
 
   devise_for :users
   root to: 'pages#home'
@@ -20,7 +13,10 @@ Rails.application.routes.draw do
   match '/messages',     to: 'messages#new',             via: 'get'
   resources "messages", only: [:new, :create]
 
-  match '/thinkerapps',  to: 'thinkerapps#new',           via: 'get'
+  match '/thinkerapps',  to: 'thinkerapps#new',          via: 'get'
   resources "thinkerapps", only: [:new, :create]
+
+  match '/researchorders',  to: 'researchorders#new',     via: 'get'
+  resources "researchorders", only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
