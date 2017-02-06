69dc578681326cb9641feaab90d9753540a4f104
Petya Nikolova <petia_182@yahoo.com>
2017-02-06 21:03:31 +0000

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206193136) do
+ActiveRecord::Schema.define(version: 20170206205549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170206193136) do
     t.boolean  "accepts_data_treatment"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "email"
   end
 
   create_table "thinker_profiles", force: :cascade do |t|
