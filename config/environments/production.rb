0445467b65a20934003016332ec37932e5c65845
Petya Nikolova <petia_182@yahoo.com>
2017-02-06 18:34:36 +0000

    updated config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,10 @@
-Rails.application.configure do
+_Rails.application.configure do
   config.action_mailer.default_url_options = { host: "lightclock.herokuapp.com" }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
   # Code is not reloaded between requests.
