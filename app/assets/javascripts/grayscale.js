2a8e358015f046c976fe5fec5e05b35c538f5ebe
Petya Nikolova <petia_182@yahoo.com>
2017-02-06 13:16:22 +0000

    commented out grayscale.js stuff

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -47,29 +47,29 @@ $(document).ready(function() {
   })
 });
 
-document.onreadystatechange = function () {
-  var state = document.readyState
-  if (state == 'interactive') {
-       document.getElementById('contents').style.visibility="hidden";
-  } else if (state == 'complete') {
-      setTimeout(function(){
-         document.getElementById('interactive');
-         document.getElementById('load').style.visibility="hidden";
-         document.getElementById('contents').style.visibility="visible";
-      },1000);
-  }
-}
+// document.onreadystatechange = function () {
+//   var state = document.readyState
+//   if (state == 'interactive') {
+//        document.getElementById('contents').style.visibility="hidden";
+//   } else if (state == 'complete') {
+//       setTimeout(function(){
+//          document.getElementById('interactive');
+//          document.getElementById('load').style.visibility="hidden";
+//          document.getElementById('contents').style.visibility="visible";
+//       },1000);
+//   }
+// }
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-  $('a.page-scroll').bind('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
-});
+// // jQuery for page scrolling feature - requires jQuery Easing plugin
+// $(function() {
+//   $('a.page-scroll').bind('click', function(event) {
+//     var $anchor = $(this);
+//     $('html, body').stop().animate({
+//       scrollTop: $($anchor.attr('href')).offset().top
+//     }, 1500, 'easeInOutExpo');
+//     event.preventDefault();
+//   });
+// });
 
 // Closes the Responsive Menu on Menu Item Click
 // $('.navbar-collapse ul li a').click(function() {
@@ -77,153 +77,153 @@ $(function() {
 // });
 
 // Google Maps Scripts
-var map = null;
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-  map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-});
+// var map = null;
+// // When the window has finished loading create our google map below
+// google.maps.event.addDomListener(window, 'load', init);
+// google.maps.event.addDomListener(window, 'resize', function() {
+//   map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+// });
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+// function init() {
+//     // Basic options for a simple Google Map
+//     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+//     var mapOptions = {
+//         // How zoomed in you want the map to start at (always required)
+//         zoom: 15,
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+//         // The latitude and longitude to center the map (always required)
+//         center: new google.maps.LatLng(40.6700, -73.9400), // New York
 
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+//         // Disables the default Google Maps UI components
+//         disableDefaultUI: true,
+//         scrollwheel: false,
+//         draggable: false,
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 17
-          }]
-        }, {
-          "featureType": "landscape",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 20
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 17
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 29
-          }, {
-            "weight": 0.2
-          }]
-        }, {
-          "featureType": "road.arterial",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 18
-          }]
-        }, {
-          "featureType": "road.local",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 16
-          }]
-        }, {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 21
-          }]
-        }, {
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "visibility": "on"
-          }, {
-            "color": "#000000"
-          }, {
-            "lightness": 16
-          }]
-        }, {
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "saturation": 36
-          }, {
-            "color": "#000000"
-          }, {
-            "lightness": 40
-          }]
-        }, {
-          "elementType": "labels.icon",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "transit",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 19
-          }]
-        }, {
-          "featureType": "administrative",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 20
-          }]
-        }, {
-          "featureType": "administrative",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#000000"
-          }, {
-            "lightness": 17
-          }, {
-            "weight": 1.2
-          }]
-        }]
-      };
+//         // How you would like to style the map.
+//         // This is where you would paste any style found on Snazzy Maps.
+//         styles: [{
+//           "featureType": "water",
+//           "elementType": "geometry",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 17
+//           }]
+//         }, {
+//           "featureType": "landscape",
+//           "elementType": "geometry",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 20
+//           }]
+//         }, {
+//           "featureType": "road.highway",
+//           "elementType": "geometry.fill",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 17
+//           }]
+//         }, {
+//           "featureType": "road.highway",
+//           "elementType": "geometry.stroke",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 29
+//           }, {
+//             "weight": 0.2
+//           }]
+//         }, {
+//           "featureType": "road.arterial",
+//           "elementType": "geometry",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 18
+//           }]
+//         }, {
+//           "featureType": "road.local",
+//           "elementType": "geometry",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 16
+//           }]
+//         }, {
+//           "featureType": "poi",
+//           "elementType": "geometry",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 21
+//           }]
+//         }, {
+//           "elementType": "labels.text.stroke",
+//           "stylers": [{
+//             "visibility": "on"
+//           }, {
+//             "color": "#000000"
+//           }, {
+//             "lightness": 16
+//           }]
+//         }, {
+//           "elementType": "labels.text.fill",
+//           "stylers": [{
+//             "saturation": 36
+//           }, {
+//             "color": "#000000"
+//           }, {
+//             "lightness": 40
+//           }]
+//         }, {
+//           "elementType": "labels.icon",
+//           "stylers": [{
+//             "visibility": "off"
+//           }]
+//         }, {
+//           "featureType": "transit",
+//           "elementType": "geometry",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 19
+//           }]
+//         }, {
+//           "featureType": "administrative",
+//           "elementType": "geometry.fill",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 20
+//           }]
+//         }, {
+//           "featureType": "administrative",
+//           "elementType": "geometry.stroke",
+//           "stylers": [{
+//             "color": "#000000"
+//           }, {
+//             "lightness": 17
+//           }, {
+//             "weight": 1.2
+//           }]
+//         }]
+//       };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+//     // Get the HTML DOM element that will contain your map
+//     // We are using a div with id="map" seen below in the <body>
+//     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
+//     // Create the Google Map using out element and options defined above
+//     map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: image
-    });
-  }
+//     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+//     var image = 'img/map-marker.png';
+//     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+//     var beachMarker = new google.maps.Marker({
+//       position: myLatLng,
+//       map: map,
+//       icon: image
+//     });
+//   }
