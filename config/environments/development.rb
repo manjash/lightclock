9be12987e0d9cd4ed6d6a9651f258bc67419444f
Petya Nikolova <petia_182@yahoo.com>
2017-02-07 12:27:11 +0000

    changes to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,14 +34,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_ADDRESS"],
-    password: ENV["GMAIL_APP_PASSWORD"]
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["GMAIL_ADDRESS"],
+  #   password: ENV["GMAIL_APP_PASSWORD"]
+  # }
 
   config.action_mailer.perform_caching = false
   # config.action_mailer.delivery_method = :letter_opener
