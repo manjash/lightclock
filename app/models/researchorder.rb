91e3c1f545d8c948e278bc990738178002895845
Petya Nikolova <petia_182@yahoo.com>
2017-02-24 11:34:39 +0000

    validations added

--- app/models/researchorder.rb
+++ app/models/researchorder.rb
@@ -1,2 +1,10 @@
 class Researchorder < ApplicationRecord
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ }, presence: true
+  validates :phone, presence: true
+  validates :organisation, presence: true
+  validates :research_subject, presence: true
+  validates :time, presence: true
 end
